22abcf9f55fffcfbec2f8598653f2f12bdc100fa
arifannyrs <arifannyrs@gmail.com>
2019-09-16 19:30:52 +0700

    Praktikum 3 Bundle Activity

--- Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/BundleActivity.java
@@ -12,6 +12,10 @@ public class BundleActivity extends AppCompatActivity {
     private EditText nameInput;
     private EditText ageInput;
 
+    public static final String USERNAME_KEY = "username";
+    public static final String NAME_KEY = "name";
+    public static final String AGE_KEY = "age";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,18 @@ public class BundleActivity extends AppCompatActivity {
         usernameInput = findViewById(R.id.input_username);
         nameInput = findViewById(R.id.input_name);
         ageInput = findViewById(R.id.input_age);
+    }
+
+    public void handleSubmit(View view) {
+        String username = usernameInput.getText().toString();
+        String name = nameInput.getText().toString();
+        int age = Integer.parseInt(ageInput.getText().toString());
+
+        Intent intent = new Intent(this, ProfileBundleActivity.class);
+        intent.putExtra(USERNAME_KEY,username);
+        intent.putExtra(NAME_KEY, name);
+        intent.putExtra(AGE_KEY, age);
+
+        startActivity(intent);
     }
 }
